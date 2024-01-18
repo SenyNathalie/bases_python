82bf8e9defe4bb27bdde42c9523b3bc08bc75624
SenyN <goumousenynat@gmail.com>
2024-01-18 19:04:09 +0100

    Exercice_4_reponse -Compter le nombre d'occurrences d'une lettre dans une chaine de caractère.

---
+++ quatre_o.py
@@ -0,0 +1,9 @@
+"""lettre_a_chercher="o"
+phrase="Bonjour tout le monde"
+dans cet exercice,nous cherchons à compter le nombre d'occurrences d'une lettre dans une chaine de caractère.
+Ici,nous cherchons le nombre de fois que la lettre o apparait dans la phrase'bonjour tout le monde'
+votre script devra retourner dans ce cas-ci dans une variable resultat le nombre 4!"""
+lettre_a_chercher = "o"
+phrase ="Bonjour tout le monde"
+resultat = phrase.count("o")
+print(resultat)
