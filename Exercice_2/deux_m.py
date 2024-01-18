978e437d8fe2c25c2e487e64fbbcd3937f9298c3
SenyN <goumousenynat@gmail.com>
2024-01-18 12:36:01 +0100

    Exercice_2_reponse -Remlacer le mot Bonjour dans la variable phrase par le mot Bonsoir.

---
+++ Exercice_2/deux_m.py
@@ -0,0 +1,7 @@
+"""phrase = "Bonjour tout le monde."
+Dans cet exercice,vous devez remplacer le mot"Bonjour"dans la variable phrase par le mot "Bonsoir") l'aide d'une méthode disponible sur les chaines de caractères.
+La variable nouvelle_phrase devra donc contenir la chaine de caractères "Bonsoir tout le monde."
+"""
+phrase = "Bonjour tout le monde."
+nouvelle_phrase ="Bonjour tout le monde.".replace("jour","soir")
+print(nouvelle_phrase)
