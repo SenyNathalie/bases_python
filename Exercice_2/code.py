57d2d99638f4c94a2ffe360787138ad31165fa5d
SenyN <goumousenynat@gmail.com>
2024-01-20 11:37:42 +0100

    Exercice_2_reponse -Remplacer le mot bonjour dans la variable phrase par le mot bonsoir.

--- Exercice_2/code.py
+++ Exercice_2/code.py
@@ -1,7 +1,8 @@
 """phrase = "Bonjour tout le monde."
-Dans cet exercice,vous devez remplacer le mot"Bonjour"dans la variable phrase par le mot "Bonsoir") l'aide d'une méthode disponible sur les chaines de caractères.
-La variable nouvelle_phrase devra donc contenir la chaine de caractères "Bonsoir tout le monde."
+Dans cet exercice, vous devez remplacer le mot "Bonjour" dans la variable phrase par le mot 
+"Bonsoir" à l'aide d'une méthode disponible sur les chaînes de caractères.
+La variable nouvelle_phrase devra donc contenir la chaîne de caractères "Bonsoir tout le monde."
 """
-phrase = "Bonjour tout le monde."
-nouvelle_phrase ="Bonjour tout le monde.".replace("jour","soir")
+phrase="Bonjour tout le monde."
+nouvelle_phrase="Bonjour tout le monde.".replace("jour","soir")
 print(nouvelle_phrase)
